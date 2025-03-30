59ca6dc59ab3fc253671b1e188480cfba0489ade
andrejskvorcov <and.skv@truelabel.io>
2025-03-30 19:39:44 +0300

    TABLE-6: edit warning i18n

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,18 +1,18 @@
+// i18n.js
 import { createI18n } from 'vue-i18n';
 import bg from '@/locales/bg.json';
 import en from '@/locales/en.json';
 import ru from '@/locales/ru.json';
 
+const messages = {
+  bg,
+  en,
+  ru
+};
+
 const i18n = createI18n({
-  legacy: false, // Используем Composition API
-  locale: 'en', // Язык по умолчанию
-  fallbackLocale: 'en', // Резервный язык
-  globalInjection: true, // Позволяет использовать $t в шаблонах
-  messages: {
-    bg,
-    en,
-    ru,
-  },
+  locale: 'en',
+  messages,
 });
 
-export { i18n };
+export default i18n;
