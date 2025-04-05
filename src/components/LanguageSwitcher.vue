52151cc7dc84d9532a6327f6e38dc013d119a48d
andrejskvorcov <and.skv@truelabel.io>
2025-04-05 16:29:33 +0300

    TABLE-8: Сделать меню для мобильной версии. Dropdown

--- src/components/LanguageSwitcher.vue
+++ src/components/LanguageSwitcher.vue
@@ -16,37 +16,8 @@
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref, watch } from 'vue'
-import { useI18n } from 'vue-i18n'
-
-const { locale } = useI18n()
-
-const languages = [
-  { code: 'en', label: 'EN' },
-  { code: 'bg', label: 'BG' },
-  { code: 'ru', label: 'RU' }
-]
-
-const currentLanguage = ref(locale.value)
-
-const changeLanguage = (lang: string) => {
-  locale.value = lang
-  localStorage.setItem('lang', lang)
-}
-
-onMounted(() => {
-  const savedLanguage = localStorage.getItem('lang')
-
-  if (savedLanguage) {
-    locale.value = savedLanguage
-    currentLanguage.value = savedLanguage
-  }
-})
-
-watch(locale, (newLang) => {
-  document.documentElement.setAttribute('lang', newLang)
-  document.body.className = `lang--${newLang}`
-}, { immediate: true })
+import { useLanguageSwitcher } from '@/composables/useLanguageSwitcher'
+const { languages, currentLanguage, changeLanguage } = useLanguageSwitcher()
 </script>
 
 <style scoped>
